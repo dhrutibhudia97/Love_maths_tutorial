3f4e1d817e54837b8b5cd453079f575e23df7ca3
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-09-16 11:36:49 +0000

    Display addiiton question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,15 +13,35 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("you clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    //want the default game to addition to be running when page loads
+    runGame("addition");
 })
-
-function runGame() {
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+// the parameter of the funGame function is gameType
+//First we pass the gameType into the function as an argument
+function runGame(gameType) {
+    //Creates two random numbers between 1 and 25
+    // worked this on on dev tools under console tab, want a number between 1 and 25 and Maths.floor rounds numbers down to closest integer, so we add one to answer so zero is not included. and so 25 is.
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    
+//Second we check if gameType parameter is equal to addition, then it will display addiiton question, otherwise it will show an error.
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
+
 
 function checkAnswer() {
 
@@ -38,8 +58,11 @@ function incrementScore() {
 function incrementWrongAnswer(){
 
 }
-
-function displayAdditionQuestions(){
+//setting its text content to our number, refers back to id's in html
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
