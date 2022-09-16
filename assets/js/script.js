cac8f26f027432be19cc4ddf74ca45f95f0e744f
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-09-16 13:26:20 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 //Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 //when button is clicked, code inside code block will run.
-// if statement... "this" refers to button that was just clicked. then we call its getAttribute
+// if statement... "this" refers to button that was just clicked. 
+//then we call its getAttribute
 //back quotes (``) are template literals
 //${game-type} tells user what game type was clicked.
 document.addEventListener("DOMContentLoaded", function() {
@@ -10,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("you clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -28,11 +29,15 @@ document.addEventListener("DOMContentLoaded", function() {
 //First we pass the gameType into the function as an argument
 function runGame(gameType) {
     //Creates two random numbers between 1 and 25
-    // worked this on on dev tools under console tab, want a number between 1 and 25 and Maths.floor rounds numbers down to closest integer, so we add one to answer so zero is not included. and so 25 is.
+    // worked this on on dev tools under console tab,
+    // want a number between 1 and 25 and Maths.floor rounds numbers down 
+    //to closest integer, so we add one to answer so zero is not included.
+    // and so 25 is.
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
     
-//Second we check if gameType parameter is equal to addition, then it will display addiiton question, otherwise it will show an error.
+//Second we check if gameType parameter is equal to addition, 
+//then it will display addiiton question, otherwise it will show an error.
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
@@ -42,13 +47,55 @@ function runGame(gameType) {
 
 }
 
-
+/**
+ * Check the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
+//we are going to target our answer box becayse we need to get a value from it.
+//we are going to take the value out of "answer-box" and store it in our
+//userAnswer variable
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+//we cehck the answer we receive from the DOM, we compare it to correct answer we 
+//get from the calculatedCorrectAnswer, it its true we congratulate user.
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
+
 
 }
 
+/**Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom and returns the correct number.
+ */
+//the first part is just getting the values back from the DOM. 
+//We are using the parseInt function to make sure we treat value as an integer.
+//by default when js returns data it returns as a string (we need it to be a number)
+//hence we are using parseInt
 function calculateCorrectAnswer(){
-
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+//if we dont recognise the operator, we will alert the user and throw an error.
+//the first part of the statement then checks the operator, if its  + sign it will 
+//calculate the correct answer
+//our function will have got the value on num1 and stored in in operand1, same with
+//num2 and + sign stored in operator.
+//then it will return an array with 2 elements [element1, element2].
+//element1 will correct correct answer operand1+operand2, 
+//element2 will be gameType we want to run next, which will be an addition game.
+    if(operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`)
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
