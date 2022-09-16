8111cfbbf0d4843ca96c44b2ce735398a1ff0ccb
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2022-09-16 10:20:40 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestions(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
+
+
